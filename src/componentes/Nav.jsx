b5ab8b04a15531012edc7964accd896da83d99a5
erikalad <erikaladner5@gmail.com>
2023-10-23 20:03:48 -0300

    estilos

--- src/componentes/Nav.jsx
+++ src/componentes/Nav.jsx
@@ -1,35 +1,44 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import './index.scss';
-import back from './../imagenes/arrow-back.svg'
+import back from './../imagenes/arrow-back.svg';
+import { Link } from 'react-router-dom';
 
 export default function Nav() {
   const location = useLocation();
 
   let textoSpan;
+  let targetRoute;
 
   switch (location.pathname) {
     case '/home':
       textoSpan = '¡Hola Test!';
+      targetRoute = '/';
       break;
     case '/awards':
       textoSpan = '¿A quién querés premiar?';
+      targetRoute = '/home';
       break;
     case '/verification':
       textoSpan = 'Nombre de la Acción';
+      targetRoute = '/';
       break;
     case '/acredit':
       textoSpan = '¿Cuánto querés ingresar?';
+      targetRoute = '/home';
       break;
     default:
       textoSpan = '';
+      targetRoute = '/';
   }
 
   return (
     <nav className="navbar">
-      {location.pathname !== "/auth" ? 
-      <img src={back} className='img-back' alt='arrow-back'/>
-    : null}
+      {(location.pathname !== "/" && location.pathname !== "/home") &&
+        <Link to={targetRoute}>
+          <img src={back} className='img-back' alt='arrow-back' />
+        </Link>
+      }
       <span className="navbar-text">{textoSpan}</span>
     </nav>
   );
